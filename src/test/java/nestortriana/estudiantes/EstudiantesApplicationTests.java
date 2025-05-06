0c85e2f8c23b6ccf2e3981709e50a8e037c1a457
Nestor Triana <ngtriana12@gmail.com>
2025-05-05 19:12:37 -0500

    inicio del proyecto

---
+++ src/test/java/nestortriana/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package nestortriana.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
